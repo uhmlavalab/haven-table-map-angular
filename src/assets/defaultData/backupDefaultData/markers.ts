beab0c87fef20e935bb85de9ba513962f3792ad3
James Hutchinson <jhutch42@hawaii.edu>
2019-08-15 11:03:41 -1000

    backed up default data

---
+++ src/assets/defaultData/backupDefaultData/markers.ts
@@ -0,0 +1,23 @@
+import { Marker } from '@app/interfaces';
+
+export const markers: Marker[] = [{
+  markerId: 3,
+  job: 'year',
+  delay: 40, 
+  minRotation: 3
+}, {
+  markerId: 6,
+  job: 'layer',
+  delay: 250, 
+  minRotation: 5
+}, {
+  markerId: 5,
+  job: 'scenario',
+  delay: 250, 
+  minRotation: 5
+}, {
+  markerId: 11,
+  job: 'add',
+  delay: 600, 
+  minRotation: 5
+}];
