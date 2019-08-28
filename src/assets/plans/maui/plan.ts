0c75a80aaa860234c75f76e0623268f6e752d2f0
James Hutchinson <jhutch42@hawaii.edu>
2019-08-27 14:27:40 -1000

    fixed issue with maui second screen

--- src/assets/plans/maui/plan.ts
+++ src/assets/plans/maui/plan.ts
@@ -8,7 +8,7 @@ export const MauiPlan: Plan = {
   displayName: 'Maui',
   landingImagePath: 'assets/plans/maui/images/landing-image.jpg',
   secondScreenImagePath: 'assets/plans/maui/images/second-screen-images/backgrounds/maui.jpg',
-  includeSecondScreen: true,
+  includeSecondScreen: false,
   selectedPlan: false,
   minYear: 2016,
   maxYear: 2045,
