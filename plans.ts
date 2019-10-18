9204c549a012118680a9fb0af3f767abb5733b70
RussMasu <46938458+RussMasu@users.noreply.github.com>
2019-10-17 17:15:39 -1000

    Delete plans.ts

--- plans.ts
+++
@@ -1,9 +0,0 @@
-import { Plan } from '@app/interfaces';
-
-import { MauiPlan } from './maui/plan';
-import { OahuPlan } from './oahu/plan';
-import { BigIslandPlan } from './bigisland/plan';
-
-export const Plans: Plan[] = [ OahuPlan, BigIslandPlan, MauiPlan ];
-
-
