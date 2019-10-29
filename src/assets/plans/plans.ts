bc66be57d16f93d43bf003b49efd200230b1e224
RussMasu <46938458+RussMasu@users.noreply.github.com>
2019-10-28 17:20:32 -1000

    Added functionality to support mauna kea plan

--- src/assets/plans/plans.ts
+++ src/assets/plans/plans.ts
@@ -1,9 +1,9 @@
 import { Plan } from '@app/interfaces';
 
-//import { MaunaKeaPlan } from './maunakea/plan';
-import { MauiPlan } from './maui/plan';
+import { MaunaKeaPlan } from './maunakea/plan';
+//import { MauiPlan } from './maui/plan';
 import { OahuPlan } from './oahu/plan';
 import { BigIslandPlan } from './bigisland/plan';
 
-//export const Plans: Plan[] = [ OahuPlan, BigIslandPlan, MaunaKeaPlan ];
-export const Plans: Plan[] = [ OahuPlan, BigIslandPlan, MauiPlan ];
+export const Plans: Plan[] = [ OahuPlan, BigIslandPlan, MaunaKeaPlan ];
+//export const Plans: Plan[] = [ OahuPlan, BigIslandPlan, MauiPlan ];
