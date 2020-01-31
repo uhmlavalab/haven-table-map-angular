661556c0744ae9cf3bf773c78908d016808ad722
Ryan Theriot <rtheriot@hawaii.edu>
2020-01-31 07:55:43 -1000

    Added preliminary arcgis map.

--- src/app/universal-components/map-element/map-element.component.ts
+++ src/app/universal-components/map-element/map-element.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { PlanService } from '@app/services/plan.service';
 import * as d3 from 'd3';
 import { MapLayer, Parcel } from '@app/interfaces';
+
+import { loadModules } from "esri-loader";
 
 @Component({
   selector: 'app-map-element',
@@ -9,7 +11,7 @@ import { MapLayer, Parcel } from '@app/interfaces';
   styleUrls: ['./map-element.component.css']
 })
 
-export class MapElementComponent implements OnInit {
+export class MapElementComponent implements OnInit, OnDestroy {
 
   scale: number;
   width: number;
@@ -23,99 +25,188 @@ export class MapElementComponent implements OnInit {
 
 
   @ViewChild('mapDiv', { static: true }) mapDiv: ElementRef;
+  view: any;
 
   constructor(private planService: PlanService) {
-    this.scale = this.planService.getMain() ? planService.getMapScale() : planService.getMiniMapScale();
-    this.width = planService.getMapImageWidth() * this.scale;
-    this.height = planService.getMapImageHeight() * this.scale;
-    this.rasterBounds = planService.getMapBounds();
-    this.baseMapImagePath = planService.getBaseMapPath();
+
+    // this.scale = this.planService.getMain() ? planService.getMapScale() : planService.getMiniMapScale();
+    // this.width = planService.getMapImageWidth() * this.scale;
+    // this.height = planService.getMapImageHeight() * this.scale;
+    // this.rasterBounds = planService.getMapBounds();
+    // this.baseMapImagePath = planService.getBaseMapPath();
   }
 
   ngOnInit() {
-    this.projection = d3.geo.mercator()
-      .scale(1)
-      .translate([0, 0]);
 
-    this.path = d3.geo.path()
-      .projection(this.projection);
+    document.addEventListener('keypress', (event) => this.logKey(event));
 
-    this.map = d3.select(this.mapDiv.nativeElement).append('svg')
-      .attr('width', this.width)
-      .attr('height', this.height);
 
-    this.map.append('image')
-      .attr('xlink:href', `${this.baseMapImagePath}`)
-      .attr('width', this.width)
-      .attr('height', this.height);
+    this.initializeMap();
+    // this.projection = d3.geo.mercator()
+    //   .scale(1)
+    //   .translate([0, 0]);
 
-    this.planService.getLayers().forEach(layer => {
-      if (layer.filePath === null) {
-        return;
-      }
-      d3.json(`${layer.filePath}`, (error, geoData) => {
-        const bounds = [this.projection(this.rasterBounds[0]), this.projection(this.rasterBounds[1])];
-        const scale = 1 / Math.max((bounds[1][0] - bounds[0][0]) / this.width, (bounds[1][1] - bounds[0][1]) / this.height);
-        const transform = [
-          (this.width - scale * (bounds[1][0] + bounds[0][0])) / 2,
-          (this.height - scale * (bounds[1][1] + bounds[0][1])) / 2
-        ] as [number, number];
+    // this.path = d3.geo.path()
+    //   .projection(this.projection);
 
-        const proj = d3.geo.mercator()
-          .scale(scale)
-          .translate(transform);
+    // this.map = d3.select(this.mapDiv.nativeElement).append('svg')
+    //   .attr('width', this.width)
+    //   .attr('height', this.height);
 
-        const path = d3.geo.path()
-          .projection(proj);
+    // this.map.append('image')
+    //   .attr('xlink:href', `${this.baseMapImagePath}`)
+    //   .attr('width', this.width)
+    //   .attr('height', this.height);
 
-        this.map.selectAll(layer.name)
-          .data(geoData.features)
-          .enter().append('path')
-          .attr('d', path)
-          .attr('class', layer.name)
-          .each(function (d) {
-            layer.parcels.push({ path: this, properties: (d.hasOwnProperty(`properties`)) ? d[`properties`] : null } as Parcel);
-          }).call(() => {
-            if (layer.setupFunction !== null) {
-              layer.setupFunction(this.planService);
-            } else {
-              this.defaultFill(layer);
-            }
-          });
-      });
-    });
+    // this.planService.getLayers().forEach(layer => {
+    //   if (layer.filePath === null) {
+    //     return;
+    //   }
+    //   d3.json(`${layer.filePath}`, (error, geoData) => {
+    //     const bounds = [this.projection(this.rasterBounds[0]), this.projection(this.rasterBounds[1])];
+    //     const scale = 1 / Math.max((bounds[1][0] - bounds[0][0]) / this.width, (bounds[1][1] - bounds[0][1]) / this.height);
+    //     const transform = [
+    //       (this.width - scale * (bounds[1][0] + bounds[0][0])) / 2,
+    //       (this.height - scale * (bounds[1][1] + bounds[0][1])) / 2
+    //     ] as [number, number];
 
-    // Subscribe to layer toggling
+    //     const proj = d3.geo.mercator()
+    //       .scale(scale)
+    //       .translate(transform);
+
+    //     const path = d3.geo.path()
+    //       .projection(proj);
+
+    //     this.map.selectAll(layer.name)
+    //       .data(geoData.features)
+    //       .enter().append('path')
+    //       .attr('d', path)
+    //       .attr('class', layer.name)
+    //       .each(function (d) {
+    //         layer.parcels.push({ path: this, properties: (d.hasOwnProperty(`properties`)) ? d[`properties`] : null } as Parcel);
+    //       }).call(() => {
+    //         if (layer.setupFunction !== null) {
+    //           layer.setupFunction(this.planService);
+    //         } else {
+    //           this.defaultFill(layer);
+    //         }
+    //       });
+    //   });
+    // });
+
+    // // Subscribe to layer toggling
     this.planService.toggleLayerSubject.subscribe(layer => {
-      if (layer) {
-        if (layer.updateFunction !== null) {
-          layer.updateFunction(this.planService);
-        } else {
-          this.defaultFill(layer);
-        }
-      }
+      console.log(layer);
+      // if (layer) {
+      //   if (layer.updateFunction !== null) {
+      //     layer.updateFunction(this.planService);
+      //   } else {
+      //     this.defaultFill(layer);
+      //   }
+      // }
     });
 
     this.planService.updateLayerSubject.subscribe(layer => {
-      if (layer) {
-        if (layer.updateFunction !== null && this.planService.getCurrentPlan()) {
-          layer.updateFunction(this.planService);
-        } else {
-          //this.defaultFill(layer);
-        }
-      }
+      console.log(layer);
+      // if (layer) {
+      //   if (layer.updateFunction !== null && this.planService.getCurrentPlan()) {
+      //     layer.updateFunction(this.planService);
+      //   } else {
+      //     //this.defaultFill(layer);
+      //   }
+      // }
     });
 
     this.planService.yearSubject.subscribe(year => {
-      if (year) {
-        const layers = this.planService.getLayers();
-        layers.forEach(layer => {
-          if (layer.updateFunction !== null && layer.active) {
-            layer.updateFunction(this.planService);
-          }
-        });
-      }
+      console.log(year);
+      // if (year) {
+      //   const layers = this.planService.getLayers();
+      //   layers.forEach(layer => {
+      //     if (layer.updateFunction !== null && layer.active) {
+      //       layer.updateFunction(this.planService);
+      //     }
+      //   });
+      // }
     });
+  }
+
+  logKey(e) {
+    let cam = this.view.camera.clone()
+    switch (e.code) {
+      case "Numpad4":
+        cam.position.longitude = this.view.camera.position.longitude - 0.001;
+        this.view.goTo(cam);
+        break;
+      case "Numpad8":
+        cam.position.latitude = this.view.camera.position.latitude + 0.001;
+        this.view.goTo(cam);
+        break;
+      case "Numpad6":
+        cam.position.longitude = this.view.camera.position.longitude + 0.001;
+        this.view.goTo(cam);
+        break;
+      case "Numpad2":
+        cam.position.latitude = this.view.camera.position.latitude - 0.001;
+        this.view.goTo(cam);
+        break;
+      case "Numpad7":
+        cam.heading = this.view.camera.heading - 1;
+        this.view.goTo(cam);
+        break;
+      case "Numpad9":
+        cam.heading = this.view.camera.heading + 1;
+        this.view.goTo(cam);
+        break;
+      case "NumpadAdd":
+        cam.position.z = this.view.camera.position.z + 1000;
+        this.view.goTo(cam);
+        break;
+      case "NumpadSubtract":
+        cam.position.z = this.view.camera.position.z - 1000;
+        this.view.goTo(cam);
+        break;
+
+    }
+  }
+
+  async initializeMap() {
+    try {
+      const [WebMap, SceneView] = await loadModules([
+        "esri/WebMap",
+        "esri/views/SceneView",
+      ]);
+
+      var webmap = new WebMap({
+        portalItem: {
+          id: "2d7ddfbe1b854b12877fcd96ea3f5b1c"
+        }
+      });
+ 
+      var view = new SceneView({
+        container: this.mapDiv.nativeElement,
+        map: webmap,
+        camera: {
+          position: { // observation point
+            x: -157.987207,
+            y: 21.449828,
+            z: 70000 // altitude in meters
+          },
+          tilt: 0 // perspective in degrees
+        }
+      });
+
+      this.view = view;
+      return this.view;
+    } catch (error) {
+      console.log("EsriLoader: ", error);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.view) {
+      // destroy the map view
+      this.view.container = null;
+    }
   }
 
   defaultFill(layer: MapLayer) {
