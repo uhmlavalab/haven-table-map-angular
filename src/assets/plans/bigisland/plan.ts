3cf9431f1eb4b3e179c3cf7dbe9b4f4dfa570de6
Drew Gotshalk <32356221+The-Black-Star@users.noreply.github.com>
2020-02-11 19:25:33 -1000

    added lava and road layers

--- src/assets/plans/bigisland/plan.ts
+++ src/assets/plans/bigisland/plan.ts
@@ -905,6 +905,80 @@ export const BigIslandPlan: Plan = {
                 });
               },
             },//end placeholder layer
+
+        {//Start Volcano
+          //It sounds cool.
+          name: 'volcanohazard',
+          displayName: 'Volcano Lava Flow Hazard Zones',
+          active: false,
+          included: true,
+          iconPath: 'assets/plans/bigisland/images/icons/fire.png',
+          secondScreenImagePath: 'assets/plans/bigisland/images/second-screen-images/layer-images/dod.jpg',
+          secondScreenText: 'Slide the Layer Puck to add or remove this layer.',
+          fillColor: mapLayerColors.Dod.fill,
+          borderColor: mapLayerColors.Dod.border,
+          borderWidth: 1,
+          legendColor: mapLayerColors.Dod.fill,
+          filePath: 'assets/plans/bigisland/layers/Volcano_Lava_Flow_Hazard_Zones.json',
+          parcels: [],
+                setupFunction(planService: PlanService) {
+                  this.parcels.forEach(parcel => {
+                      d3.select(parcel.path)
+                        .style('fill', 'transparent')
+                        .style('opacity', (this.active) ? 0.85 : 0.0)
+                        .style('stroke', this.borderColor)
+                        .style('stroke-width', (this.borderWidth+2) + 'px');
+                  });
+                },
+                updateFunction(planService: PlanService) {
+                  this.parcels.forEach(parcel => {
+                    let layerattribute = parcel.properties.type;//divide based on layer attribute 
+
+                        d3.select(parcel.path)
+                          .style('fill', 'transparent')//set to Colors if fill wanted, otherwise transparent
+                          .style('opacity', this.active ? 0.85 : 0.0)
+                          .style('stroke','#FF0000' )//set to borderColors if borders wanted otherwise this.bordercolor
+                          .style('stroke-width', (this.borderWidth+ 2)  + 'px');
+                  });
+                },
+        },//end volcano layer
+    
+        {//Start Volcano
+          //It sounds cool.
+          name: 'majorroads',
+          displayName: 'Major Roads Hawaii County',
+          active: false,
+          included: true,
+          iconPath: 'assets/plans/bigisland/images/icons/fire.png',
+          secondScreenImagePath: 'assets/plans/bigisland/images/second-screen-images/layer-images/dod.jpg',
+          secondScreenText: 'Slide the Layer Puck to add or remove this layer.',
+          fillColor: mapLayerColors.Dod.fill,
+          borderColor: mapLayerColors.Dod.border,
+          borderWidth: 1,
+          legendColor: mapLayerColors.Dod.fill,
+          filePath: 'assets/plans/bigisland/layers/Major_Roads__Hawaii_County.json',
+          parcels: [],
+                setupFunction(planService: PlanService) {
+                  this.parcels.forEach(parcel => {
+                      d3.select(parcel.path)
+                        .style('fill', 'transparent')
+                        .style('opacity', (this.active) ? 0.85 : 0.0)
+                        .style('stroke', this.borderColor)
+                        .style('stroke-width', (this.borderWidth+5) + 'px');
+                  });
+                },
+                updateFunction(planService: PlanService) {
+                  this.parcels.forEach(parcel => {
+                    let layerattribute = parcel.properties.type;//divide based on layer attribute 
+
+                        d3.select(parcel.path)
+                          .style('fill', 'transparent')//set to Colors if fill wanted, otherwise transparent
+                          .style('opacity', this.active ? 1 : 0.0)
+                          .style('stroke','#FFFFFF' )//set to borderColors if borders wanted otherwise this.bordercolor
+                          .style('stroke-width', (this.borderWidth+5) + 'px');
+                  });
+                },
+        }//end volcano layer
     ],
   }
 }
