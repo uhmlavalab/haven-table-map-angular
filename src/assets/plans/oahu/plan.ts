ea8bfe3772627635ab4686fea277df4feff1162a
James Hutchinson <jhutch42@hawaii.edu>
2022-03-10 13:14:20 -1000

    small changes

--- src/assets/plans/oahu/plan.ts
+++ src/assets/plans/oahu/plan.ts
@@ -72,7 +72,7 @@ export const OahuPlan: Plan = {
     colors: chartColors
   },
   map: {
-    scale: 0.237,
+    scale: 0.2735,
     width: 3613,
     height: 2794,
     bounds: [[-158.281, 21.710], [-157.647, 21.252]],
